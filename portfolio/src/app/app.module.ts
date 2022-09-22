a41d58e2c5432d12648c0a3cf0e881d53615eaef
Ricardo Federico Barzola <99059530+pincha1212@users.noreply.github.com>
2022-09-22 13:51:53 -0300

    agregamos banner y social

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SocialComponent } from './components/social/social.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LogoAPComponent,
     HeaderComponent,
-    SocialComponent
+    SocialComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
