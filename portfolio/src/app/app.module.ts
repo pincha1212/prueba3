ebf5e56d14edd151772983e6bc367370d2115656
Ricardo Federico Barzola <99059530+pincha1212@users.noreply.github.com>
2022-09-23 00:27:47 -0300

    terminamos el footer

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { EduComponent } from './components/edu/edu.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HysComponent } from './components/hys/hys.component';
 import { ProyectoComponent } from './components/proyecto/proyecto.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { ProyectoComponent } from './components/proyecto/proyecto.component';
     ExperienciaComponent,
     EduComponent,
     HysComponent,
-    ProyectoComponent
+    ProyectoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
